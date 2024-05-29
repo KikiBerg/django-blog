ecd122b3e61c7d3dfbdf23656c570b92ef442ce3
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-05-29 14:59:43 +0000

    Gitpod migration

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-kikiberg-djangoblog-rg3bui5d82g.ws-eu114.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-kikiberg-djangoblog-9gdo39esy23.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 
@@ -92,8 +92,10 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.gitpod.io",
+    #"https://*.gitpod.io",
+    "https://*.codeinstitute-ide.net",
     "https://*.herokuapp.com"
+    
 ]
 
 
